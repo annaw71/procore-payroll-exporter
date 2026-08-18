47c09cfb147871be30eca62cafe109d693cf61bf
Anna <anna@csgcva.com>
2026-08-18 14:17:29 -0400

    changed access token

--- app.py
+++ app.py
@@ -259,7 +259,7 @@ if (
                 failed_projects,
             ) = get_timecards(
                 api_url=api_url,
-                procore_access_token=st.session_state["procore_access_token"],
+                access_token=st.session_state["procore_access_token"],
                 company_id=st.session_state["company_id"],
                 projects=st.session_state["projects"],
                 start_date=start_date,
