64210b9029af9209b21d47beb4e2435ff0bb8dc9
Anna <anna@csgcva.com>
2026-08-18 14:48:25 -0400

    e

--- app.py
+++ app.py
@@ -89,9 +89,15 @@ else:
         redirect_uri=redirect_uri,
     )
 
-    st.link_button(
-        "Authorize Procore",
-        authorization_url,
+    st.markdown(
+        f"""
+    <a href="{authorization_url}" target="_self">
+        <button>
+            Authorize Procore
+        </button>
+    </a>
+    """,
+        unsafe_allow_html=True,
     )
 
 # ============================================================
